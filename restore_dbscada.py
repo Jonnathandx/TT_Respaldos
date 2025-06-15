7eb762168c135def377ae77d89c0326efe6b5422
Jonnathan Nieves <137567777+Jonnathandx@users.noreply.github.com>
2025-06-15 18:47:08 -0500

    Add files via upload

---
+++ restore_dbscada.py
@@ -0,0 +1,5 @@
+#!/bin/env python3.12
+
+import os
+
+os.system('PGPASSWORD="gNZ1R#BhcYLjaad@*Ry" psql -h 192.168.222.62 -U administrador -d Datos_SCADA -f /home/administrador/scripts/db_scada.sql')

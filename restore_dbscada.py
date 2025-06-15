f5d4e55d2f155412ee1222f8ebe9b0b496193df5
Jonnathan Nieves <137567777+Jonnathandx@users.noreply.github.com>
2025-06-15 18:47:45 -0500

    Update restore_dbscada.py

--- restore_dbscada.py
+++ restore_dbscada.py
@@ -2,4 +2,4 @@
 
 import os
 
-os.system('PGPASSWORD="gNZ1R#BhcYLjaad@*Ry" psql -h 192.168.222.62 -U administrador -d Datos_SCADA -f /home/administrador/scripts/db_scada.sql')
+os.system('PGPASSWORD="**********" psql -h 192.168.222.62 -U administrador -d Datos_SCADA -f /home/administrador/scripts/db_scada.sql')
